aa384c45d707f5fdef73543b9400353832beec3f
Mathisfoe <m.foe2010@gmail.com>
2026-09-14 12:07:16 +0200

    moyenne

---
+++ moyenne.py
@@ -0,0 +1,8 @@
+print("Saisi cinq notes d’évaluations sur 20")
+nbr1=float(input())
+nbr2=float(input())
+nbr3=float(input())
+nbr4=float(input())
+nbr5=float(input())
+moyenne=(nbr1+nbr2+nbr3+nbr4+nbr5)/5
+print(moyenne)
