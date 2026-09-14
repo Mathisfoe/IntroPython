33edcde2b92aa424bc1a1515423a7f1ee8efb568
Mathisfoe <m.foe2010@gmail.com>
2026-09-14 11:54:43 +0200

    aire_rectangle

---
+++ aire_rectangle.py
@@ -0,0 +1,6 @@
+print("Longueur d'un rectangle")
+longueurrectangle = float(input())
+print("Largeur du rectangle")
+largeurrectangle = float(input())
+airerectangle =longueurrectangle*largeurrectangle
+print(airerectangle)
